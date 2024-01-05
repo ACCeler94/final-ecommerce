3084c70a292aebad34bc8701cc5ede9d64d0980f
Artur C <araghast94@icloud.com>
2024-01-05 16:26:51 +0100

    Add get endpoints for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get('/:id')
+  async getProductById(@Param('id', new ParseUUIDPipe()) id: 'string') {
+    const product = await this.productsService.getProductById(id);
+    if (!product) throw new NotFoundException('Product not found.');
+
+    return product;
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAllProducts(): Promise<Product[]> {
+    return this.prismaService.product.findMany();
+  }
+
+  public getProductById(id: Product['id']): Promise<Product> | null {
+    return this.prismaService.product.findUnique({ where: { id } });
+  }
+}
