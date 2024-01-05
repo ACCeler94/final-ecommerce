8ee81699fdff8469edfa6e4a5b858565e97fc9d5
Artur C <araghast94@icloud.com>
2024-01-05 16:01:34 +0100

    Add prisma.module and prisma.service, add validationPipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import * as cors from 'cors';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, PrismaModule],
   controllers: [AppController, ProductsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+  await app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
