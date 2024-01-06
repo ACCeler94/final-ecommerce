75786af8c0b6ea53932b886eb3014bbd2b3297c9
Artur C <araghast94@icloud.com>
2024-01-06 15:44:00 +0100

    Add methods and endpoints for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  getAllUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Get('/:id')
+  getUserById(@Param('id', new ParseUUIDPipe()) id: 'string') {
+    return this.usersService.getUserById(id);
+  }
+
+  // [TODO] Add auth guard to allow only self deletion
+  @Delete('/:id')
+  async deleteUser(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    await this.usersService.deleteUser(id);
+    return { message: 'success' };
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(prismaService: PrismaService) {}
+  constructor(private prismaService: PrismaService) {}
+
+  public getUsers(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  public getUserById(id): Promise<User> {
+    return this.prismaService.user.findUnique({ where: { id } });
+  }
+
+  public async createUser(userData: Omit<User, 'id'>): Promise<User> {
+    try {
+      return await this.prismaService.user.create({ data: userData });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Email is already taken');
+
+      throw error;
+    }
+  }
+
+  public async updateUser(userData, id: User['id']): Promise<User> {
+    try {
+      return await this.prismaService.user.update({
+        where: { id },
+        data: userData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Email is already taken');
+
+      throw error;
+    }
+  }
+
+  public async deleteUser(id: User['id']): Promise<User> {
+    return this.prismaService.user.delete({ where: { id } });
+  }
 }
