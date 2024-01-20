1dad4f8501589b7db92ae61d427e6c47f853b70c
Artur C <araghast94@icloud.com>
2024-01-20 12:18:07 +0100

    Add message for an empty cart

--- client/src/components/features/Cart/Cart.tsx
+++ client/src/components/features/Cart/Cart.tsx
@@ -1,24 +1,34 @@
 import { useSelector } from 'react-redux';
-import { RootState, useAppDispatch } from '../../../store/store';
+import { RootState } from '../../../store/store';
 import CartItem from '../CartItem/CartItem';
 import styles from './Cart.module.css';
+import Button from '../../common/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
   const cart = useSelector((state: RootState) => state.cart.shoppingCart);
+  const navigate = useNavigate();
+
+  if (cart.length === 0) {
+    return (
+      <div className={styles.emptyCartWrapper}>
+        <h3>The Cart is empty.</h3>
+        <Button buttonText="Go back" buttonHandler={() => navigate(-1)} />
+      </div>
+    );
+  }
 
   return (
-    <div className={styles.cartWrapper}>
-      <div className="cart-product-list">
-        <ul>
-          {cart.map((cartObj) => {
-            return (
-              <li key={cartObj.product.id} className={styles.cartItemWrapper}>
-                <CartItem {...cartObj} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="cart-product-list">
+      <ul>
+        {cart.map((cartObj) => {
+          return (
+            <li key={cartObj.product.id} className={styles.cartItemWrapper}>
+              <CartItem {...cartObj} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
