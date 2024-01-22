8925179573c79145b2b7148a0160bfd1810bfe7e
Artur C <araghast94@icloud.com>
2024-01-22 19:53:44 +0100

    Add placeOrder method in productsAPI

--- client/src/API/productsApi.ts
+++ client/src/API/productsApi.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import { API_URL } from './config';
+import Order from '../types/Order';
 
 const productsAPI = {
   // get requests
   fetchAll: () => axios.get(`${API_URL}/products`),
   fetchById: (id: string) => axios.get(`${API_URL}/products/${id}`),
+
+  // post requests
+  placeOrder: (orderData: Order) => axios.post(`${API_URL}/`, orderData),
 };
 
 export default productsAPI;
