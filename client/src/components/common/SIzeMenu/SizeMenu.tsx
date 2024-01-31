8ba463e7264b04e7667977de184e6bbdc0a72e09
Artur C <araghast94@icloud.com>
2024-01-31 12:18:25 +0100

    Unify quantity input field size, add id and key to SizeMenu select and options

--- client/src/components/common/SIzeMenu/SizeMenu.tsx
+++ client/src/components/common/SIzeMenu/SizeMenu.tsx
@@ -1,15 +1,27 @@
+import styles from './SizeMenu.module.css';
+import { nanoid } from 'nanoid';
+
 interface SizeMenuProps {
   changeHandler: (size: string) => void;
   sizes: string;
+  productId: string;
 }
 
-const SizeMenu = ({ changeHandler, sizes }: SizeMenuProps) => {
+const SizeMenu = ({ changeHandler, sizes, productId }: SizeMenuProps) => {
   const sizesArr = sizes.split(', ');
 
   return (
-    <select id="size-select" onChange={(e) => changeHandler(e.target.value)}>
+    <select
+      id={`size-select-${productId}`}
+      className={styles.sizeSelect}
+      onChange={(e) => changeHandler(e.target.value)}
+    >
       {sizesArr.map((size) => {
-        return <option value={size}>{size}</option>;
+        return (
+          <option value={size} key={nanoid()}>
+            {size.toUpperCase()}
+          </option>
+        );
       })}
     </select>
   );
