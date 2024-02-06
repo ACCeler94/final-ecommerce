8af1fae6eaf7b1675fa2551bd525d4cea0db21bb
Artur C <araghast94@icloud.com>
2024-02-06 21:24:32 +0100

    Add custom hook to create multi step form

---
+++ client/src/utils/useMultistepForm.ts
@@ -0,0 +1,29 @@
+import { ReactElement, useState } from 'react';
+
+export const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const next = () => {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  };
+
+  const back = () => {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  };
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    next,
+    back,
+  };
+};
