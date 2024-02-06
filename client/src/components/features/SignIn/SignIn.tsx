c88bbddc38865511efe131890526534119483660
Artur C <araghast94@icloud.com>
2024-02-06 14:37:25 +0100

    Add border, button and basic functionality (without submit) to SignIn

--- client/src/components/features/SignIn/SignIn.tsx
+++ client/src/components/features/SignIn/SignIn.tsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
 import styles from './SignIn.module.css';
+import Button from '../../common/Button/Button';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
+  const [userLogin, setUserLogin] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
   return (
     <section>
-      <form className={styles.signInForm}>
-        <label htmlFor="login">Login</label>
-        <input type="text" id="login" name="login" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-      </form>
+      <div className={styles.formWrapper}>
+        <h1>Sign In</h1>
+        <form className={styles.signInForm}>
+          <label htmlFor="login">Login</label>
+          <input
+            type="text"
+            id="login"
+            name="login"
+            value={userLogin}
+            onChange={(e) => setUserLogin(e.target.value)}
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={userPassword}
+            onChange={(e) => setUserPassword(e.target.value)}
+            required
+          />
+          <Button buttonHandler={() => {}} buttonText="Sign In" />
+        </form>
+        <p>
+          No account? <Link to="/account/register">Register</Link>
+        </p>
+      </div>
     </section>
   );
 };
