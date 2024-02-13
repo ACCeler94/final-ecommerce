36d43b630be1c4fd9efa34977406042cdde8a678
Artur C <araghast94@icloud.com>
2024-02-13 14:34:47 +0100

    rename usersApi to authApi, add loginUser method

--- client/src/API/authApi.ts
+++ client/src/API/authApi.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import { API_URL } from './config';
 import User from '../types/User';
+import LoginData from '../types/LoginData';
 
-const usersAPI = {
+const authAPI = {
   // post requests
   registerUser: (userData: User) =>
     axios.post(`${API_URL}/auth/register`, userData),
+
+  loginUser: (loginData: LoginData) =>
+    axios.post(`${API_URL}/auth/login`, loginData),
 };
 
-export default usersAPI;
+export default authAPI;
