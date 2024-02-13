525bf9221f2324fc6bdec820f7864101dd079cc6
Artur C <araghast94@icloud.com>
2024-02-13 13:20:15 +0100

    Add styling, fix incorrect redirect

--- client/src/components/pages/RegisterSuccessPage/RegisterSuccessPage.tsx
+++ client/src/components/pages/RegisterSuccessPage/RegisterSuccessPage.tsx
@@ -1,15 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../../common/Button/Button';
 import Container from '../../common/Container/Container';
+import styles from './RegisterSuccessPage.module.css';
 
 const RegisterSuccessPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="checkout-page">
+    <main className={styles.registerSuccess}>
       <Container>
         <h1>You have been successfully registered!</h1>
-        <Button buttonText="Login" buttonHandler={() => navigate('success')} />
+        <Button buttonText="Login" buttonHandler={() => navigate('/login')} />
       </Container>
     </main>
   );
