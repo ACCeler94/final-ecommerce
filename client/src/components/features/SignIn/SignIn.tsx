29daf3de521e3a1039d6a9a65288b9a9208f6c75
Artur C <araghast94@icloud.com>
2024-02-16 15:35:59 +0100

    Change login field type to email

--- client/src/components/features/SignIn/SignIn.tsx
+++ client/src/components/features/SignIn/SignIn.tsx
@@ -24,7 +24,7 @@ const SignIn = () => {
         <form className={styles.signInForm} onSubmit={submitHandler}>
           <label htmlFor="login">Login</label>
           <input
-            type="text"
+            type="email"
             id="login"
             name="login"
             value={userLogin}
