3c21cd16720a62ae4afd6db578418a88d60a6cbf
Artur C <araghast94@icloud.com>
2024-02-16 15:29:05 +0100

    Add submitHandler to sign in form, add error handling

--- client/src/components/features/SignIn/SignIn.tsx
+++ client/src/components/features/SignIn/SignIn.tsx
@@ -2,16 +2,26 @@ import { useState } from 'react';
 import styles from './SignIn.module.css';
 import Button from '../../common/Button/Button';
 import { Link } from 'react-router-dom';
+import { RootState, useAppDispatch } from '../../../store/store';
+import { fetchLogIn } from './SignInSlice';
+import { useSelector } from 'react-redux';
 
 const SignIn = () => {
   const [userLogin, setUserLogin] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const dispatch = useAppDispatch();
+  const error = useSelector((state: RootState) => state.signIn.error);
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(fetchLogIn({ email: userLogin, password: userPassword }));
+  };
 
   return (
     <section>
       <div className={styles.formWrapper}>
         <h1>Sign In</h1>
-        <form className={styles.signInForm}>
+        <form className={styles.signInForm} onSubmit={submitHandler}>
           <label htmlFor="login">Login</label>
           <input
             type="text"
@@ -21,6 +31,14 @@ const SignIn = () => {
             onChange={(e) => setUserLogin(e.target.value)}
             required
           />
+          {error && error.statusCode === 401 ? (
+            <span className={styles.errorText}>Invalid login or password!</span>
+          ) : null}
+          {error && error.statusCode !== 401 ? (
+            <span className={styles.errorText}>
+              Something went wrong. Please try again...
+            </span>
+          ) : null}
           <label htmlFor="password">Password</label>
           <input
             type="password"
