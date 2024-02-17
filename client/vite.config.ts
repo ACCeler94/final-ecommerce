d30ebb71411015dfc42b5cbc29b879c15e6ce69e
Artur C <araghast94@icloud.com>
2024-02-17 17:55:22 +0100

    Add comment explaining vite proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // proxy added to simulate client and backend on the same port - this solves issues with cross site cookies not saving (for jwt token)
       '/api': 'http://localhost:3000',
     },
   },
