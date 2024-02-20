263687a3fedc971d959289d2c0476eb0d798d49e
Artur C <araghast94@icloud.com>
2024-02-20 20:20:09 +0100

    Remove password from AccountData type

--- client/src/types/AccountData.ts
+++ client/src/types/AccountData.ts
@@ -3,8 +3,6 @@ import Order from './Order';
 export type AccountData = {
   id: string;
   email: string;
-  password: string;
-  repeatPassword: string;
   name: string;
   street: string;
   zip: string;
