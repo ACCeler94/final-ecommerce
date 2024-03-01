b6e5003ad7d3de684070b31d440efc65bcf6eab9
Artur C <acceler945@gmail.com>
2024-03-01 14:12:12 +0100

    Add MobileMenu and DesktopMenu to Navbar

--- client/src/components/layout/Navbar/Navbar.tsx
+++ client/src/components/layout/Navbar/Navbar.tsx
@@ -1,16 +1,12 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from '../../common/Container/Container';
 import styles from './Navbar.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
-import CartCounter from '../../features/cart/components/CartCounter/CartCounter';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../store/store';
-import Button from '../../common/Button/Button';
+import DesktopMenu from '../DesktopMenu/DesktopMenu';
+import MobileMenu from '../MobileMenu/MobileMenu';
+import useWindowWidth from '../../../utils/useWindowWidth';
 
 const Navbar = () => {
-  const userId = useSelector((state: RootState) => state.signIn.userId);
-  const navigate = useNavigate();
+  const windowWidth = useWindowWidth();
 
   return (
     <nav className={styles.navbar}>
@@ -19,38 +15,8 @@ const Navbar = () => {
           <div className={styles.logo}>
             <Link to="/">Sartorial Selections </Link>
           </div>
-          <div className={styles.navLinksWrapper}>
-            <NavLink
-              to="/category/female"
-              className={({ isActive }) =>
-                isActive ? styles.active + ' ' + styles.navLink : styles.navLink
-              }
-            >
-              Female
-            </NavLink>
-            <NavLink
-              to="/category/male"
-              className={({ isActive }) =>
-                isActive ? styles.active + ' ' + styles.navLink : styles.navLink
-              }
-            >
-              Male
-            </NavLink>
-            <Link to="/cart" className={styles.icon}>
-              <FontAwesomeIcon icon={faCartShopping} />
-              <CartCounter />
-            </Link>
-            {userId ? (
-              <Link to="/account/my-account" className={styles.icon}>
-                <FontAwesomeIcon icon={faUser} />{' '}
-              </Link>
-            ) : (
-              <Button
-                buttonText="Sign In"
-                buttonHandler={() => navigate('/account/sign-in')}
-              />
-            )}
-          </div>
+          {/* Conditional rendering, instead of queries, mitigates the issue when the scroll is disabled on the body while the mobile menu is opened. Otherwise, resizing the window would lock the scroll without any way to close the menu */}
+          <div>{windowWidth <= 768 ? <MobileMenu /> : <DesktopMenu />}</div>
         </div>
       </Container>
     </nav>
